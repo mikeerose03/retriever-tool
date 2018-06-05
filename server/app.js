9d05ec164d8d5cdec3ead5000535897691169055
Mikee Rose I. Bersabal <mribersabal@vauldex.com>
2018-06-05 11:33:55 +0800

    Setup development environment for client and server sides

---
+++ server/app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+const {sequelize} = require('./models')
+
+const app = express()
+app.use(morgan('combined'))
+app.use(bodyParser.json())
+app.use(cors())
+
+require('./routes')(app)
+
+sequelize.sync()
+  .then(() => {
+    app.listen(process.env.PORT || 8081)
+    console.log(`Server started on port, ${process.env.PORT || 8081}`)
+  })
